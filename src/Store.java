bea14e6c2d0263932f626972aedb67287b4516e1
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-06-13 09:11:51 +0200

    #4 fix NullPointer exception

--- src/Store.java
+++ src/Store.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 import model.IArticle;
 
 public class Store {
 
 	private List<IArticle> articles;
+	
+	public Store() {
+		articles = new ArrayList<>();
+	}
 	
 	public void addArticle(IArticle article) {
 		articles.add(article);
