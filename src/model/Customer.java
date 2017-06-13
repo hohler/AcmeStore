abbaf768592f5b2bcb4b61b7ec3c4f5890831049
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-06-13 09:45:14 +0200

    #7 added more properties to Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -4,6 +4,9 @@ public class Customer {
 
 	private String firstName;
 	private String lastName;
+	private int postCode;
+	private String town;
+	private String street;
 	
 	public Customer() {}
 	
@@ -24,9 +27,34 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+		
+	public int getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(int postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
 	@Override
 	public String toString() {
-		return "[Customer] First name: "+firstName+", last name: "+lastName;
+		return "[Customer] First name: "+firstName+", last name: "
+				+lastName+", street: "+street+", post code: "+postCode+", town: "+town;
 	}
 }
